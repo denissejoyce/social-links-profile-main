4e0c0b3e9754b9bc7e0ef12834792dfb3abb0d98
denissejoyce <denisse.alido@gmail.com>
2024-12-26 13:27:36 -0600

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
-function displayTxt() {
-  alert("hey");
-}
+// function changeMode() {
+//   //   document.getElementsByTagName("body").classList.add("bg--night");
+//   document.getElementByID(t).classList.remove("toggler--day");
+//   document.getElementByID(t).classList.add("toggler--night");
+// }
