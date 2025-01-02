c26c58fcf0b5d0d2824946d1504b101fbbe28aa8
denissejoyce <denisse.alido@gmail.com>
2025-01-02 13:15:06 -0600

    🚀 update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 "use strict";
 
+let dayMode = 1;
+const mode = document.querySelector(".mode");
+mode.innerHTML = "Night";
+
 function changeMode() {
   const body = document.body;
+  if (dayMode > 0) {
+    dayMode -= 1;
+    mode.innerText = "Day";
+  } else {
+    dayMode += 1;
+    mode.innerText = "Night";
+  }
 
   body.classList.toggle("bg--day");
   body.classList.toggle("bg--night");
